27a24932793d4daf7b72c2eea515d5102f7f1418
Eugene Agafonov <e.a.agafonov@gmail.com>
2026-04-02 19:08:01 -0700

    fix: quote all variable expansions in cloc-commit.sh
    All variable expansions in cloc-commit.sh:13-25 are unquoted. Paths with
    spaces will break mkdir, cloc, git log, jq, and mv. Every ${VAR} should
    be "${VAR}".
    
    Closes: cloc-trends-irz

--- cloc-commit.sh
+++ cloc-commit.sh
@@ -4,20 +4,20 @@
 set -e
 set -u
 
-REPO_PATH=${1}
-CLOC_DIR=${2}
-COMMIT=${3}
+REPO_PATH="${1}"
+CLOC_DIR="${2}"
+COMMIT="${3}"
 
-CLOC_FILE=${CLOC_DIR}/${COMMIT}.json
+CLOC_FILE="${CLOC_DIR}/${COMMIT}.json"
 
-cd ${REPO_PATH}
-cloc --json --out=${CLOC_FILE} ${COMMIT}
+cd "${REPO_PATH}"
+cloc --json --out="${CLOC_FILE}" "${COMMIT}"
 
-AUTHOR_DATE=$(git log -1 --format="%aI" ${COMMIT})
-COMMIT_DATE=$(git log -1 --format="%cI" ${COMMIT})
+AUTHOR_DATE=$(git log -1 --format="%aI" "${COMMIT}")
+COMMIT_DATE=$(git log -1 --format="%cI" "${COMMIT}")
 
 jq --arg author_date "${AUTHOR_DATE}" --arg commit_date "${COMMIT_DATE}" \
   '. + { commit: {author_date: $author_date, commit_date: $commit_date} }' \
-  ${CLOC_FILE} > ${CLOC_FILE}.tmp
+  "${CLOC_FILE}" > "${CLOC_FILE}.tmp"
 
-mv -f ${CLOC_FILE}.tmp ${CLOC_FILE}
+mv -f "${CLOC_FILE}.tmp" "${CLOC_FILE}"
